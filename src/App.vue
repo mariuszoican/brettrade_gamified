5d34efe71e740bb3b1c9999a39ef7e498a25575a
Francisco Kahil <codexico@gmail.com>
2021-10-28 12:00:52 +0300

    done with nongamified

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,58 @@
 <template>
-  <v-app>
+  <v-app app>
     <input type="hidden" :value="currentPrice" name="exit_price" />
-    <v-app-bar app  >
-      <v-card elevation="4" v-if="currentPrice"   min-width="200">
-        <v-card-title>
-          <span class="word-break">Current price:</span>
-        </v-card-title>
-        <v-card-text class="text-h5 font-weight-bold">
-          <h2>
-            {{ tweenedPrice || currentPrice }}
-          </h2>
-        </v-card-text>
-      </v-card>
+    <v-app-bar app clipped-left>
+      <v-sheet outlined class="d-flex align-center ml-1 pa-2 rounded-xl">
+        <div class="d-flex align-center  font-weight-bold ">
+          Current price:
+          <div class="ml-1 pa-2 red   white--text text-no-wrap rounded-pill">
+            ${{ tweenedPrice || currentPrice }}
+          </div>
+        </div>
+      </v-sheet>
+
       <v-spacer></v-spacer>
-      <v-btn @click="dialog=true">Sell</v-btn>
+      <v-btn @click="dialog = true">Sell</v-btn>
     </v-app-bar>
+    <end-dialog
+      :dialog="showEndDialog"
+      @finishGame='finishGame'
+      
+    ></end-dialog>
+
     <confirm-dialog
       :dialog="dialog"
       :currentPrice="currentPrice"
       @sell="sell"
       @continueKeeping="continueKeeping"
     ></confirm-dialog>
-    <v-main>
+    <v-navigation-drawer clipped app width="300" color="blue" permanent>
+      <v-card
+        class="d-flex flex-column buysellcard"
+        fill-height
+        style="height:100%"
+      >
+        <v-card-text class="overflow-y-auto" style="margin-bottom:50px">
+          <v-list>
+            <v-list-item-group active-class="border" color="indigo">
+              <v-list-item
+                v-for="(item, i) in messages"
+                :key="i"
+                :id="`li_${i}`"
+                :ref="`li_${i}`"
+                dense
+              >
+                <v-list-item-content>
+                  <v-list-item-title v-text="item"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+          <div id="listend" ref="listend">&nbsp</div>
+        </v-card-text>
+      </v-card>
+    </v-navigation-drawer>
+    <v-main app>
       <v-container fluid>
         <v-row>
           <v-col cols="12">
@@ -29,7 +60,7 @@
               :constructorType="'stockChart'"
               class="hc"
               :options="chartOptions"
-              ref="chart1"
+              ref="priceGraph"
               :updateArgs="[true, true, true]"
             ></highcharts>
           </v-col>
@@ -43,11 +74,13 @@
 /* eslint-disable */
 
 
+import EndDialog from "./components/EndDialog";
 import ConfirmDialog from "./components/ConfirmDialog";
 import { Chart } from "highcharts-vue";
 import { differenceInSeconds } from "date-fns";
 import gsap from "gsap";
 import _ from "lodash";
+import add from "date-fns/fp/add/index.js";
 
 const formatDown = {
   color: "red",
@@ -63,9 +96,13 @@ export default {
   components: {
     highcharts: Chart,
     ConfirmDialog,
+    EndDialog
   },
   data: function() {
     return {
+      showEndDialog:false,
+      messages: [],
+      probToZero: 0.1,
       startingPrice: 100,
       currentPrice: 100,
       submittable: false,
@@ -76,7 +113,7 @@ export default {
       dialog: false,
       tweenedPrice: null,
       stockInterval: null,
-      tickFrequency: 3,
+      tickFrequency: 1,
       chartOptions: {
         xAxis: {
           max: 100,
@@ -97,8 +134,6 @@ export default {
     };
   },
   computed: {
-     
-
     formattedTween() {
       return this.tweenedPrice.toFixed(2);
     },
@@ -106,11 +141,13 @@ export default {
   watch: {
     async submittable(val) {
       if (val) {
-        // await this.sendMessage({ name: "Trade_ends" });
+        await this.sendMessage({ name: "Trade_ends" });
+        this.showEndDialog=true;
         // document.getElementById("form").submit();
       }
     },
     currentPrice: function(newValue) {
+      this.$refs.listend.scrollIntoView({ behavior: "smooth" });
       gsap.to(this.$data, {
         duration: 0.5,
         tweenedPrice: newValue,
@@ -118,21 +155,43 @@ export default {
       });
     },
   },
-  async created() {
-    this.$options.sockets.onopen = async () =>
-      await this.sendMessage({ name: "Trade_starts" });
-    this.$options.sockets.onmessage = (data) => console.log(data);
-  },
+  async created() {},
   async mounted() {
+    this.$nextTick(() => {
+      this.$refs.listend.scrollIntoView({ behavior: "smooth" });
+
+      this.$refs.priceGraph.chart.setSize(null, window.innerHeight - 150);
+      // this.$refs.priceGraph.chart.reflow();
+    });
     this.stockInterval = setInterval(async () => {
-      if (!this.dialog) {
+      if (!this.dialog && !this.submittable) {
         const addendum = _.random(0, 2);
-        this.currentPrice+=addendum
+        this.currentPrice += addendum;
+        const r = _.random(0, 1, true);
+        if (r < this.probToZero) {
+          this.submittable = true;
+          this.currentPrice = 0;
+        }
+        this.addMessage(this.currentPrice, addendum);
+
         this.chartOptions.series[0].data.push(this.currentPrice);
       }
-    }, this.tickFrequency * 2000);
+    }, this.tickFrequency * 1000);
   },
   methods: {
+    finishGame(){
+      console.log('game finished')
+    },
+    addMessage(price, addendum) {
+      if (price === 0) {
+        const msg = `Game ends!`;
+        this.messages.push(msg);
+      }
+      if (addendum > 0 && !this.dialog && !this.submittable) {
+        const msg = `Price growth: + ${addendum}`;
+        this.messages.push(msg);
+      }
+    },
     async sendMessage(obj) {},
     tweenUpd(v) {
       this.tweenedPrice = _.round(this.tweenedPrice, 2);
@@ -150,7 +209,7 @@ export default {
     async continueKeeping() {
       await this.sendMessage({ name: "continueKeeping" });
       this.dialog = false;
-      this.submittable = true;
+      this.submittable = false;
     },
   },
 };
@@ -158,14 +217,27 @@ export default {
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  /* font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 .word-break {
   word-break: break-word;
+}
+
+.listouter1 {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.listouter2 {
+  flex: 1 1 auto;
+  display: flex;
+  flex-direction: column;
+  /* flex-direction: column-reverse; */
+  overflow-y: scroll;
 }
 </style>
---
+++ src/components/EndDialog.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-dialog v-model="dialog" persistent max-width="490">
+    <v-card>
+      <v-card-title class="text-h5" color="danger"
+        >End of the round</v-card-title
+      >
+
+      <v-card-text>
+        Click 'Next' to continue to the next round
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+         
+          
+            <v-btn color="red darken-1" @click="submit"> Next</v-btn>
+           
+        
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: ["dialog"],
+  components: {},
+  data: () => ({}),
+
+  methods: {
+    submit() {
+      this.$emit("finishGame");
+    },
+  },
+};
+</script>
