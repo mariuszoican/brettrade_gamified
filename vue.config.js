47afc36699fbdea2086805a669d1cb43277ec41f
Francisco Kahil <codexico@gmail.com>
2021-10-28 18:58:30 +0300

    done with upd vue config path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/crossmerged/" : "/",
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/brettrade_gamified/" : "/",
   lintOnSave: false,
   transpileDependencies: ["vuetify"],
 };
